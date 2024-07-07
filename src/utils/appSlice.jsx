929c4cabc09d5daa6ef1d0a0608addf80fc9eb49
AbdulNaved <140192855+AbdulNaved@users.noreply.github.com>
2024-07-07 14:37:33 -0500

    update

--- src/utils/appSlice.jsx
+++ src/utils/appSlice.jsx
@@ -27,35 +27,3 @@ const appSlice = createSlice({
 export const { togglesidebar, setHomeVideo, setCategory, setSearchSuggestion } = appSlice.actions;
 export default appSlice.reducer;
 
-
-
-// import { createSlice, configureStore } from '@reduxjs/toolkit'
-
-// const appSlice = createSlice({
-//   name: 'app',
-//   initialState: {
-//     open: true,
-//     video: [],
-//     category: "All",
-//     searchSuggestion: [],
-//   },
-//   reducers: {
-//     // actions
-//     togglesidebar:(state) => {
-//       state.open = !state.open;
-//     },
-//     setHomeVideo: (state, action) => {
-//       state.video = action.payload;
-//     },
-//     setCategory: (state, action) => {
-//       state.Category = action.payload;
-//     },
-//     setSearchSuggestion: (state, action) => {
-//       state.searchSuggestion = action.payload;
-//     }
-
-//   }
-
-// });
-// export const {togglesidebar, setHomeVideo, setCategory,  setSearchSuggestion} = appSlice.actions;
-// export default appSlice.reducer;
