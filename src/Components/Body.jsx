ed87b0ce889b33b94bb1ab08fdde06fd59c76b3d
AbdulNaved <140192855+AbdulNaved@users.noreply.github.com>
2025-01-22 14:54:47 -0500

    Update Body.jsx

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,19 +1,15 @@
-import React from 'react'
-import Sidebar from './Sidebar'
-import Feed from './Feed'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import Sidebar from './Sidebar';
+import Feed from './Feed';
+import {Outlet} from 'react-router-dom';
+ 
 
-function Body() {
+const Body = () => {
   return (
-    <div>
-        <div className="flex mt-16">
-         <Sidebar/>
-         {/* video render here  */}
-          <Feed/>
-          <Outlet/>
-          {}
+    <div className="flex mt-16">
+        <Sidebar />
+        <Outlet/>
       </div>
-    </div>
   )
 }
 
