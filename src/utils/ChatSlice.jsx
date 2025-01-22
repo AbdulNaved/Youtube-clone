3643a27e344e9e525e618466be25aaee054923dd
AbdulNaved <140192855+AbdulNaved@users.noreply.github.com>
2025-01-22 15:04:25 -0500

    Update ChatSlice.jsx

--- src/utils/ChatSlice.jsx
+++ src/utils/ChatSlice.jsx
@@ -1,20 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {createSlice} from "@reduxjs/toolkit";
 
 const chatSlice = createSlice({
-    name: "chat",
-    initialState: {
-        messages: [],
+    name:"chat",
+    initialState:{
+        message:[],
     },
-    reducers: {
-        setMessage: (state, action) => {
-            if (state.messages.length >= 100) {
-                state.messages.shift(); // Ensure we don't have more than 100 messages
-            }
-            state.messages.push(action.payload);
-        }
+    reducers:{
+        setMessage:(state,action)=>{
+            state.message.splice(100,1);
+            state.message.push(action.payload);
+        } 
     }
-});
-
-export const { setMessage } = chatSlice.actions;
+})
+export const {setMessage} = chatSlice.actions;
 export default chatSlice.reducer; 
-
