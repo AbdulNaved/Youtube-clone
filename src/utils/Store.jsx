9e693237ddd033bf141aedc5c7eed56f7f2bc21f
AbdulNaved <140192855+AbdulNaved@users.noreply.github.com>
2025-01-22 15:03:16 -0500

    Update Store.jsx

--- src/utils/Store.jsx
+++ src/utils/Store.jsx
@@ -1,13 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {configureStore} from "@reduxjs/toolkit";
 import appReducer from "./appSlice";
-import chatReducer from './ChatSlice'; // Corrected import
+import chatReducer from "./chatSlice";
 
 const store = configureStore({
-    reducer: {
-        app: appReducer,
-        chat: chatReducer // Corrected reducer naming
+    reducer:{
+        app:appReducer,
+        chat:chatReducer
     }
-});
-
+})
 export default store;
-
