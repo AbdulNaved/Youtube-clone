b4d8bda11bb423c855167acaeaf75f00b3474b96
AbdulNaved <140192855+AbdulNaved@users.noreply.github.com>
2025-01-22 14:55:51 -0500

    Update ChatMessege.jsx

--- src/Components/ChatMessege.jsx
+++ src/Components/ChatMessege.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import Avatar from 'react-avatar';
+import Avatar from "react-avatar";
 
-function ChatMessage({ item }) {
+const ChatMessage = ({item}) => {
   return (
-    <div className='flex items-start space-x-2 py-2'>
-      <Avatar
-        src="https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg?auto=compress&cs=tinysrgb&w=600"
-        className="cursor-pointer"
-        size={25}
-        round={true}
-      />
-      <div>
-        <h1 className='font-bold text-sm'>{item.name}</h1>
-        <p className='text-sm'>{item.message}</p>
-      </div>
+    <div className='flex items-center'>
+        <div>
+        <Avatar src="https://play-lh.googleusercontent.com/C9CAt9tZr8SSi4zKCxhQc9v4I6AOTqRmnLchsu1wVDQL0gsQ3fmbCVgQmOVM1zPru8UH=w240-h480-rw" size={25} round={true} />
+                            
+        </div>
+        <div className='flex items-center'>
+            <h1 className='ml-2 font-bold text-sm'>{item.name}</h1>
+            <p className='ml-2 py-2 text-sm'>{item.message}</p>
+        </div>
     </div>
-  );
+  )
 }
 
-export default ChatMessage;
-
-
-
+export default ChatMessage
